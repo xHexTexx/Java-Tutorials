fbe59a8e492bd79ea0c356a4130896d6cca04c93
Meaw_zap <tee.hemjinda@gmail.com>
2022-05-04 15:20:54 +0700

    Java

--- Java/hello.java
+++ Java/hello.java
@@ -1,7 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         
+        System.out.print("Hello World!");
         System.out.println("Hello World!") ;
+        System.out.print("Hello World!") ;
         
     }
 
---
+++ Java/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    
+    public static void main(String[] args) {
+        
+        int x = 10 , y = 10 ;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x + y);
+
+    }
+}
