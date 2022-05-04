2b96a18633f21869ee31d82a902b4f5cf38d8bfe
Meaw_zap <tee.hemjinda@gmail.com>
2022-05-04 15:53:26 +0700

    .

---
+++ Java/datatypes.java
@@ -0,0 +1,26 @@
+public class datatypes {
+    public static void main(String[] args){
+
+        short shrt = 50 ;
+        System.out.println(shrt);
+
+        int x = 10 ;
+        System.out.println(x) ;
+
+        long l = 1000000000000000000l ;
+        System.out.println(l) ;
+
+        float f = 3.14f ;
+        System.out.println(f) ;
+
+        double d = 10.23123012390193293d ;
+        System.out.println(d) ;
+
+        String s = "Hello" ;
+        System.out.println(s) ;
+
+        boolean b = true ; 
+        System.out.println(b) ;
+
+    }
+}
