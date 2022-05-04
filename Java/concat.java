20ec72fa437257e667421529a62880cbc8b837f4
Meaw_zap <tee.hemjinda@gmail.com>
2022-05-04 15:40:46 +0700

    .

---
+++ Java/concat.java
@@ -0,0 +1,17 @@
+public class concat {
+    
+    public static void main(String[] args){
+
+        String name = "Tee" ;
+        String lname = "Hemjinda" ;
+        
+        System.out.println(name + " " + lname);
+
+        String fname = name + lname ;
+
+        System.out.println(fname);
+
+        System.out.println(name.concat(lname)) ;
+    }
+
+}
