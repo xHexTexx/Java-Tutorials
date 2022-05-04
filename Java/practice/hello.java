00747bcc2284f70248295b1331895afebd659f71
Meaw_zap <tee.hemjinda@gmail.com>
2022-05-04 15:31:54 +0700

    change

--- Java/practice/hello.java
+++ Java/practice/hello.java
@@ -1,3 +1,4 @@
+package practice;
 public class hello {
     public static void main(String[] args) {
         
--- Java/practice/variables.java
+++ Java/practice/variables.java
@@ -1,3 +1,4 @@
+package practice;
 public class variables {
     
     public static void main(String[] args) {
@@ -7,5 +8,12 @@ public class variables {
         System.out.println(y);
         System.out.println(x + y);
 
+        String s = "Hello" ;
+        String ss = s + s ;
+        System.out.println(ss) ;
+
+        float f = 3.14f;
+        System.out.println(f) ;
+        
     }
 }
